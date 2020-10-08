bd2a906b712140f3c158fb8fe996daf9a841a1b2
younggg96 <yangguannggeng960123@gmail.com>
2020-10-08 02:05:10 -0400

    new update

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const glide = new Glide(".glide", { autoplay: 4000, type: 'carousel', startAt: 0, });
+const glide = new Glide(".glide", { autoplay: 4000, type: 'carousel', startAt: 1, gap: 0 });
+const captionEl = document.querySelectorAll('.slide-caption');
+
+glide.on(["mount.after", "run.after"], () => {
+    const caption = captionEl[glide.index]
+    anime({
+        targets: caption.children,
+        opacity: [0, 1],
+        duration: 400,
+        easing: "spring",
+        delay: anime.stagger(500, { start: 300 }),
+        translateY: [anime.stagger([40, 10]), 0]
+    })
+});
+
+glide.on("run.bebfore", () => {
+    document.querySelectorAll(".slide-caption > *").forEach(el => {
+        el.style.opacity = 0;
+    });
+});
 
 glide.mount();
