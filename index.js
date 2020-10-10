6576a3807151e7f3e68222b3e22a5faea1a0aa75
younggg96 <yangguannggeng960123@gmail.com>
2020-10-10 01:13:25 -0400

    new update

--- index.js
+++ index.js
@@ -24,4 +24,33 @@ glide.mount();
 ScrollReveal().reveal('.intro-title',  {reset: true, duration: 2000, delay: 500 } );
 ScrollReveal().reveal('.intro-content',  {reset: true, duration: 1000,  delay: 800 } );
 ScrollReveal().reveal('.github',  {reset: true, duration: 500, delay: 1000 } );
-ScrollReveal().reveal('.social-icons',  {reset: true, duration: 500, delay: 1200 } );
+ScrollReveal().reveal('.social-icons', { reset: true, duration: 500, delay: 1200 });
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+});
