d159fbd025f81a57ca6b02e8f2cd31fc4735a608
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2020-10-11 14:34:25 -0400

    new update

--- index.js
+++ index.js
@@ -21,36 +21,57 @@ glide.on("run.before", () => {
 
 glide.mount();
 
-ScrollReveal().reveal('.intro-title',  {reset: true, duration: 2000, delay: 500 } );
-ScrollReveal().reveal('.intro-content',  {reset: true, duration: 1000,  delay: 800 } );
-ScrollReveal().reveal('.github',  {reset: true, duration: 500, delay: 1000 } );
-ScrollReveal().reveal('.social-icons', { reset: true, duration: 500, delay: 1200 });
+
+
+ScrollReveal().reveal('.intro-title',  { duration: 2000, delay: 500 } );
+ScrollReveal().reveal('.intro-content',  { duration: 1000,  delay: 800 } );
+ScrollReveal().reveal('.github',  { duration: 500, delay: 1000 } );
+ScrollReveal().reveal('.social-icons', { duration: 500, delay: 1200 });
+
+// const workList = document.querySelectorAll('.feature-title');
+// ScrollReveal().reveal('.feature-title',  { duration: 500, delay: 1000});
 
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'radar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['HTML5', 'CSS', 'JavaScript', 'SQL', 'Java', 'Swift', 'Python'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
+            label: 'Programmign Languages',
+            data: [10, 9, 9, 6, 8, 5, 4],
+            backgroundColor: 'rgba(55, 55, 55, 0.5)',
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
+    },
+    options: {
+        legend: {
+            labels: {
+                // This more specific font property overrides the global property
+                fontColor: 'black',
+                defualtFontSize: '24px',
+            }
+        }
+    }
+});
+
+const isocope = new Isotope(".protoflio-content", {
+    layoutMode: "fitRows",
+    itemSelector: ".protoflio-item"
+})
+
+const fbtn = document.querySelector(".filter-btns");
+
+fbtn.addEventListener("click", e => {
+    let {target} = e;
+    const filterOption = target.getAttribute("data-filter");
+    if(filterOption) {
+        document.querySelectorAll(".filter-btn.active")
+        .forEach(btn => btn.classList.remove("active"));
+        target.classList.add("active");
+
+        isocope.arrange({ filter: filterOption });
     }
 });
